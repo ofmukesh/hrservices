e067bb20869d150cb0134eacf798468071326ff7
x1mukesh <of.mukesh@gmail.com>
2023-09-07 21:34:01 +0530

    hh

--- utils/common.py
+++ utils/common.py
@@ -1,16 +1,22 @@
 import uuid
 
+
 def generate_uuid_with_prefix(prefix):
-    return prefix+uuid.uuid4().hex[:10].upper()
+    key = prefix+uuid.uuid4().hex[:10].upper()
+    print(key)
+    return key
+
 
 def generate_uuid():
     return uuid.uuid4()
 
+
 def generate_uuid_6():
     return uuid.uuid4().hex[:6]
+
 
 def generate_uuid_10():
     return uuid.uuid4().hex[:10]
 
 
-low_balance_err="Insufficient funds to complete this transaction. Please add money to your wallet."
+low_balance_err = "Insufficient funds to complete this transaction. Please add money to your wallet."
