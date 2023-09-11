d47a0ef97a89030b4353993d19bda07560824fae
x1mukesh <of.mukesh@gmail.com>
2023-09-11 20:32:22 +0530

    account err

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-from utils.common import generate_uuid_10
 
 
 # Create your models here.
 class Account(models.Model):
-    id = models.CharField(
-        max_length=10, default=generate_uuid_10(), primary_key=True, editable=False)
     user = models.OneToOneField('auth.user', on_delete=models.CASCADE)
     balance = models.IntegerField(default=0, null=False, blank=False)
     contact_no = models.CharField(max_length=10, null=False, blank=False)
