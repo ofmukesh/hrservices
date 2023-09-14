d4966a7ea6990828b829dd0bb98b87de8f6c913a
x1mukesh <of.mukesh@gmail.com>
2023-09-14 07:48:07 +0530

    up

--- admin_services/views.py
+++ admin_services/views.py
@@ -22,6 +22,13 @@ class VoterMakerView(LoginRequiredMixin, AccessMixin, View):
         if form.is_valid():
             form.save()
             data=VoterRegistration.objects.get(id=form.instance.id)
+            data.date = data.date_of_registration.strftime('%d.%m.%Y')
+            data.elector_name_hi =eth(data.elector_name)
+            data.father_mother_husband_name_hi =eth(data.father_mother_husband_name)
+            data.sex_hi =eth(data.sex)
+            data.address1_hi =eth(data.address1)
+            data.address2_hi =eth(data.address2)
+            data.place_hi =eth(data.place)
             return render(request, 'admin/pages/old_voter.html', context={'title': 'Voter', 'data': data})
         else:
             request.err = form.errors
