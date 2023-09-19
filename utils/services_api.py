e6e4ff475b0ce0f8d6d7ac3c1b4fd00924f9a64f
x1mukesh <of.mukesh@gmail.com>
2023-09-19 17:19:45 +0530

    update api

--- utils/services_api.py
+++ utils/services_api.py
@@ -1,8 +1,8 @@
 import requests
 from django.views.decorators.csrf import csrf_exempt
-import os
 import environ
 from services.models import ServiceKeys
+import json
 
 env = environ.Env()
 environ.Env.read_env()
@@ -12,7 +12,7 @@ def aadhar_to_pan_api(request, aadhaar_no):
     api_key = ServiceKeys.objects.get(id='AADHAR_TO_PAN_KEY').api_key
 
     # API URL
-    api_url = f"https://neofind.in/admin/api/index.php?api_key={api_key}&aadhaar_no={aadhaar_no}"
+    api_url = f"https://neofind.in/admin/api/index.php?application_no=$application_no&api_key={api_key}&aadhaar_no={aadhaar_no}"
 
     # Make the API call
     response = requests.post(api_url)
