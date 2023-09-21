96f5badb655c2edc3716dc10f308262ba213cdc5
x1mukesh <of.mukesh@gmail.com>
2023-09-21 08:06:57 +0530

    application_no arg err

--- utils/services_api.py
+++ utils/services_api.py
@@ -7,11 +7,11 @@ env = environ.Env()
 environ.Env.read_env()
 
 @csrf_exempt
-def aadhar_to_pan_api(request, aadhaar_no):
+def aadhar_to_pan_api(request, aadhaar_no,application_no):
     api_key = ServiceKeys.objects.get(id='AADHAR_TO_PAN_KEY').api_key
 
     # API URL
-    api_url = f"https://neofind.in/admin/api/index.php?application_no=$application_no&api_key={api_key}&aadhaar_no={aadhaar_no}"
+    api_url = f"https://neofind.in/admin/api/index.php?application_no={application_no}&api_key={api_key}&aadhaar_no={aadhaar_no}"
 
     # Make the API call
     response = requests.post(api_url)
